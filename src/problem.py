891a51dca8a16c8ad40f7f982dba99abacd8f0ff
Charles Jameson <cwj@CWJ-Macbook-Pro.ns.bigpond.net.au>
2018-09-05 16:48:25 +1000

    done

--- src/problem.py
+++ src/problem.py
@@ -1,4 +1,4 @@
-import os
+import os, shutil
 import execute
 
 binaries_cache = {}
@@ -21,71 +21,53 @@ class Problem:
         with open(directory + tc, "r") as f:
             self.task_code = f.read()
 
+    async def recompile_from_cpp(self, ef, sf):
+        assert(os.path.isfile(sf))
+        if os.path.isfile(ef) and os.path.getmtime(ef) > os.path.getmtime(sf): return
+        box = execute.Box()
+        with open(sf, "rb") as sc:
+            cfile = box.prepfile('source.cpp', sc.read())
+        efile = box.prepfile('a.exe')
+        await box.run_command_async('g++ -O2 -std=c++11 -o %s %s' % (efile, cfile), timeout=10)
+        shutil.copy2(efile,ef)
+        box.cleanup()
+    
     async def load_executables(self, se, be, coe, che):
         directory = os.path.dirname(__file__) + "/problems/"
 
-        box = execute.Box()
-
         # Sanity
-        with open(directory + se, "rb") as f:
+        se = directory + se
+        if se.endswith('.cpp'):
+            sf = se
+            se = sf[:-4] + '.exe'
+            await self.recompile_from_cpp(se, sf)
+        with open(se, "rb") as f:
             self.sanity_exe = f.read()
 
-        if se.endswith('.cpp'):
-            if se in binaries_cache.keys():
-                self.sanity_exe = binaries_cache[se]
-            else:
-                cfile = box.prepfile('source.cpp', self.sanity_exe)
-                efile = box.prepfile('a.exe')
-                await box.run_command_async('g++ -O2 -std=c++11 -o %s %s' % (efile, cfile), timout=10)
-                with open(efile, "rb") as f:
-                    self.sanity_exe = f.read()
-                binaries_cache[se] = self.sanity_exe
-
         # Broken
-        with open(directory + be, "rb") as f:
+        be = directory + be
+        if be.endswith('.cpp'):
+            sf = be
+            be = sf[:-4] + '.exe'
+            await self.recompile_from_cpp(be, sf)
+        with open(be, "rb") as f:
             self.broken_exe = f.read()
 
-        if be.endswith('.cpp'):
-            if be in binaries_cache.keys():
-                self.broken_exe = binaries_cache[be]
-            else:
-                cfile = box.prepfile('source.cpp', self.broken_exe)
-                efile = box.prepfile('a.exe')
-                await box.run_command_async('g++ -O2 -std=c++11 -o %s %s' % (efile, cfile), timeout=10)
-                with open(efile, "rb") as f:
-                    self.broken_exe = f.read()
-                binaries_cache[be] = self.broken_exe
-
         # Correct
-        with open(directory + coe, "rb") as f:
-            self.correct_exe = f.read()
-
+        coe = directory + coe
         if coe.endswith('.cpp'):
-            if coe in binaries_cache.keys():
-                self.correct_exe = binaries_cache[coe]
-            else:
-                cfile = box.prepfile('source.cpp', self.correct_exe)
-                efile = box.prepfile('a.exe')
-                await box.run_command_async('g++ -O2 -std=c++11 -o %s %s' % (efile, cfile), timeout=10)
-                with open(efile, "rb") as f:
-                    self.correct_exe = f.read()
-                binaries_cache[coe] = self.correct_exe
+            sf = coe
+            coe = sf[:-4] + '.exe'
+            await self.recompile_from_cpp(coe, sf)
+        with open(coe, "rb") as f:
+            self.sanity_exe = f.read()
 
         # Checker
         if che == '': che = 'match_file'
-        with open(directory + che, "rb") as f:
-            self.checker_exe = f.read()
-
+        che = directory + che
         if che.endswith('.cpp'):
-            if che in binaries_cache.keys():
-                self.checker_exe = binaries_cache[che]
-            else:
-                cfile = box.prepfile('source.cpp', self.checker_exe)
-                efile = box.prepfile('a.exe')
-                await box.run_command_async('g++ -O2 -std=c++11 -o %s %s' % (efile, cfile), timeout=10)
-                with open(efile, "rb") as f:
-                    self.checker_exe = f.read()
-                binaries_cache[che] = self.checker_exe
-
-
-        box.cleanup()
+            sf = che
+            che = sf[:-4] + '.exe'
+            await self.recompile_from_cpp(che, sf)
+        with open(che, "rb") as f:
+            self.sanity_exe = f.read()
--- src/problems/coconut/broken1.cpp
+++ src/problems/coconut/broken1.cpp
@@ -6,7 +6,7 @@ using namespace std;
 const char* anss[] = {"no", "yes"};
 
 int main() {
-    int xi, yi, di, xc, yc, dc;
+    int xi,yi,di,xc,yc,dc;
     cin >> xi >> yi >> di >> xc >> yc >> dc;
     double d = sqrt((xi-xc)*(xi-xc) + (yi-yc)*(yi-yc));
     cout << anss[abs(d-di) <= dc+1e-8] << endl;
