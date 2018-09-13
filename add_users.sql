6cab48759544da58acbfdb9b15c32e49962ba0e3
Charles Jameson <cwj@CWJ-Macbook-Pro.ns.bigpond.net.au>
2018-09-14 00:13:56 +1000

    shuffled around db init

---
+++ add_users.sql
@@ -0,0 +1,26 @@
+TRUNCATE users;
+
+INSERT INTO users(username, password, visible, admin) VALUES ('tutors', 'simmeringaioli', FALSE, TRUE);
+INSERT INTO users(username, password, visible, admin) VALUES ('test1', 'password1', FALSE, FALSE);
+INSERT INTO users(username, password, visible, admin) VALUES ('test2', 'password2', FALSE, FALSE);
+
+INSERT INTO users(username, password, visible, admin) VALUES ('jerry', 'monash3150', TRUE, FALSE);
+INSERT INTO users(username, password, visible, admin) VALUES ('1021839', '1021839A', TRUE, FALSE);
+INSERT INTO users(username, password, visible, admin) VALUES ('samtoday', 'plaintestSAM', TRUE, FALSE);
+INSERT INTO users(username, password, visible, admin) VALUES ('pony_joe1', 'Angusage14', TRUE, FALSE);
+INSERT INTO users(username, password, visible, admin) VALUES ('mzhao', 'info1234', TRUE, FALSE);
+INSERT INTO users(username, password, visible, admin) VALUES ('jl58261', 'carrots21', TRUE, FALSE);
+INSERT INTO users(username, password, visible, admin) VALUES ('i_am_sb', 'iamsherlocked', TRUE, FALSE);
+INSERT INTO users(username, password, visible, admin) VALUES ('j_p_smith', '64scOne\', TRUE, FALSE);
+INSERT INTO users(username, password, visible, admin) VALUES ('jamesburgess', 'Aelianeditor', TRUE, FALSE);
+INSERT INTO users(username, password, visible, admin) VALUES ('jojo197', 'Scorpion&', TRUE, FALSE);
+INSERT INTO users(username, password, visible, admin) VALUES ('hfc', '311190c3', TRUE, FALSE);
+INSERT INTO users(username, password, visible, admin) VALUES ('println_hi_', '07ff08be', TRUE, FALSE);
+INSERT INTO users(username, password, visible, admin) VALUES ('mckinleykeys', '393b9dbc', TRUE, FALSE);
+INSERT INTO users(username, password, visible, admin) VALUES ('jinwoopark0', 'f1b9c7c5', TRUE, FALSE);
+INSERT INTO users(username, password, visible, admin) VALUES ('super_aiderton', '22scOne\', TRUE, FALSE);
+INSERT INTO users(username, password, visible, admin) VALUES ('blendersleuth', 'walnut88', TRUE, FALSE);
+INSERT INTO users(username, password, visible, admin) VALUES ('ivansolo2', 'acd64b16', TRUE, FALSE);
+INSERT INTO users(username, password, visible, admin) VALUES ('technocoder', 'fab30f42', TRUE, FALSE);
+INSERT INTO users(username, password, visible, admin) VALUES ('maayan', 'b7e3cd13', TRUE, FALSE);
+INSERT INTO users(username, password, visible, admin) VALUES ('spyofthemonth', '6461c132', TRUE, FALSE);
--- table_setup.sql
+++ table_setup.sql
@@ -1,4 +1,4 @@
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
 	username VARCHAR UNIQUE,
 	password VARCHAR,
 	admin BOOLEAN,
@@ -6,7 +6,7 @@ CREATE TABLE users (
 	PRIMARY KEY (username)
 );
 
-CREATE TABLE results (
+CREATE TABLE IF NOT EXISTS results (
 	id SERIAL UNIQUE,
 	owner VARCHAR,
 	score INT,
@@ -19,34 +19,11 @@ CREATE TABLE results (
 	FOREIGN KEY (owner) REFERENCES users(username)
 );
 
-INSERT INTO users(username, password, visible, admin) VALUES ('tutors', 'simmeringaioli', FALSE, TRUE);
-
-INSERT INTO users(username, password, visible, admin) VALUES ('jaz', 'blah', FALSE, FALSE);
-INSERT INTO users(username, password, visible, admin) VALUES ('jerry', 'monash3150', TRUE, FALSE);
-INSERT INTO users(username, password, visible, admin) VALUES ('1021839', '1021839A', TRUE, FALSE);
-INSERT INTO users(username, password, visible, admin) VALUES ('samtoday', 'plaintestSAM', TRUE, FALSE);
-INSERT INTO users(username, password, visible, admin) VALUES ('pony_joe1', 'Angusage14', TRUE, FALSE);
-INSERT INTO users(username, password, visible, admin) VALUES ('mzhao', 'info1234', TRUE, FALSE);
-INSERT INTO users(username, password, visible, admin) VALUES ('jl58261', 'carrots21', TRUE, FALSE);
-INSERT INTO users(username, password, visible, admin) VALUES ('i_am_sb', 'iamsherlocked', TRUE, FALSE);
-INSERT INTO users(username, password, visible, admin) VALUES ('j_p_smith', '64scOne\', TRUE, FALSE);
-INSERT INTO users(username, password, visible, admin) VALUES ('jamesburgess', 'Aelianeditor', TRUE, FALSE);
-INSERT INTO users(username, password, visible, admin) VALUES ('jojo197', 'Scorpion&', TRUE, FALSE);
-INSERT INTO users(username, password, visible, admin) VALUES ('hfc', '311190c3', TRUE, FALSE);
-INSERT INTO users(username, password, visible, admin) VALUES ('println_hi_', '07ff08be', TRUE, FALSE);
-INSERT INTO users(username, password, visible, admin) VALUES ('mckinleykeys', '393b9dbc', TRUE, FALSE);
-INSERT INTO users(username, password, visible, admin) VALUES ('jinwoopark0', 'f1b9c7c5', TRUE, FALSE);
-INSERT INTO users(username, password, visible, admin) VALUES ('super_aiderton', '22scOne\', TRUE, FALSE);
-INSERT INTO users(username, password, visible, admin) VALUES ('blendersleuth', 'walnut88', TRUE, FALSE);
-INSERT INTO users(username, password, visible, admin) VALUES ('ivansolo2', 'acd64b16', TRUE, FALSE);
-INSERT INTO users(username, password, visible, admin) VALUES ('technocoder', 'fab30f42', TRUE, FALSE);
-INSERT INTO users(username, password, visible, admin) VALUES ('maayan', 'b7e3cd13', TRUE, FALSE);
-INSERT INTO users(username, password, visible, admin) VALUES ('spyofthemonth', '6461c132', TRUE, FALSE);
-
-CREATE TABLE settings (
-	name VARCHAR UNIQUE,
-	value INT
-);
-
-INSERT INTO settings(name, value) VALUES ('submissions_allowed', 0);
-INSERT INTO settings(name, value) VALUES ('scoreboard_freeze_id', 100000000);
+IF NOT EXISTS (SELECT 1 FROM pg_catalog.pg_tables WHERE tablename = 'settings') THEN
+    CREATE TABLE settings (
+	   name VARCHAR UNIQUE,
+	   value INT
+    );
+    INSERT INTO settings(name, value) VALUES ('submissions_allowed', 0);
+    INSERT INTO settings(name, value) VALUES ('scoreboard_freeze_id', 100000000);
+END IF;
