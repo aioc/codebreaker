1abeda710787ebd21f9a1b76a47d791b34f8f1a5
Charles Jameson <cwj@CWJ-Macbook-Pro.ns.bigpond.net.au>
2018-09-20 13:11:45 +1000

    added psql username to scripts

--- scripts/contestant_access.sh
+++ scripts/contestant_access.sh
@@ -1,1 +1,1 @@
-psql -d codebreaker -c "UPDATE settings SET value = $1 WHERE name = 'contestant_access';"
+psql -U codebreaker -d codebreaker -c "UPDATE settings SET value = $1 WHERE name = 'contestant_access';"
--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 mkdir -p temp/
-psql -d codebreaker -f table_setup.sql 2> /dev/null
+psql -U codebreaker -d codebreaker -f table_setup.sql 2> /dev/null
 
