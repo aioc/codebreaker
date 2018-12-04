aeefa90676a1d8fd2f87e72aa84eea4d1e258419
Charles Jameson <cwj@dhcp-10-249-74-157.eduroam.wireless.private.cam.ac.uk>
2018-12-04 12:17:36 +0000

    removed files

--- problems/cloud/correct.cpp
+++ problems/cloud/correct.cpp
@@ -4,8 +4,6 @@ using namespace std;
 int N, K, a[100005], ans = 2e9;
 
 int main() {
-    freopen("cloudin.txt", "r", stdin);
-    freopen("cloudout.txt", "w", stdout);
     scanf("%d %d", &N, &K);
     for (int i = 1; i < N; i++) scanf("%d", a+i), a[i] += a[i-1];
     for (int i = 0; i+K < N; i++) ans = min(ans, a[i+K]-a[i]);
