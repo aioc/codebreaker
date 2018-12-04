a9682d65a11c20dda15a2c6031173bd4eabed0cd
Charles Jameson <cwj@dhcp-10-249-74-157.eduroam.wireless.private.cam.ac.uk>
2018-12-04 15:10:54 +0000

    fixed detective

--- problems/detective/1.cpp
+++ problems/detective/1.cpp
@@ -45,108 +45,108 @@ void dfs(int at, int my_colour, int my_leader)
 	}
 }
 
-int main() {
-    // Step 1. Do input
-    scanf("%d%d", &n, &m);
-    for (int i = 0;i < m;i++)
-    {
-        int a, b, c; scanf("%d%d%d", &a, &b, &c);
-        --a; --b;
-        if (c == 3)
-            claims[a].push_back(b);
-        else
-        {
-            edges[a].push_back(make_pair(b, c));
-            edges[b].push_back(make_pair(a, c));
-        }
-    }
+int main()
+	// Step 1. Do input
+	scanf("%d%d", &n, &m);
+	for (int i = 0;i < m;i++)
+	{
+		int a, b, c; scanf("%d%d%d", &a, &b, &c);
+		--a; --b;
+		if (c == 3)
+			claims[a].push_back(b);
+		else
+		{
+			edges[a].push_back(make_pair(b, c));
+			edges[b].push_back(make_pair(a, c));
+		}
+	}
 
-    // Step 2. Run a DFS to clean up some logic, set group leaders
-    for (int i = 0;i < n;i++)
-        colour[i] = UNDEFINED;
-    for (int i = 0;i < n;i++) if (colour[i] == UNDEFINED) 
-        dfs(i, 0, i);
-    for (int i = 0;i < n;i++)
-        belong[leader[i]].push_back(i);
+	// Step 2. Run a DFS to clean up some logic, set group leaders
+	for (int i = 0;i < n;i++)
+		colour[i] = UNDEFINED;
+	for (int i = 0;i < n;i++) if (colour[i] == UNDEFINED) 
+		dfs(i, 0, i);
+	for (int i = 0;i < n;i++)
+		belong[leader[i]].push_back(i);
 
-    // Step 3. Initialise stuff
-    for (int i = 0;i < n;i++)
-    {
-        possible[i] = true; // this person is a possible culprit
-        type[i] = UNDEFINED; // this group type is unknown
-    }
+	// Step 3. Initialise stuff
+	for (int i = 0;i < n;i++)
+	{
+		possible[i] = true; // this person is a possible culprit
+		type[i] = UNDEFINED; // this group type is unknown
+	}
 
-    // Step 4. Figure out what each group is claiming
-    for (int i = 0;i < n;i++) for (int u: claims[i])
-        if (colour[i] == colour[leader[i]])
-            group_claims[leader[i]][1].insert(u);
-        else
-            group_claims[leader[i]][0].insert(u);
-    for (int i = 0;i < n;i++)
-    {
-        if (group_claims[i][0].size() > 1 && group_claims[i][1].size() > 1)
-            mistake();
-        if (group_claims[i][0].size() > 1)
-        {
-            type[i] = 1;
-            for (int u: group_claims[i][0])
-                possible[u] = false;
-        }
-        if (group_claims[i][1].size() > 1)
-        {
-            type[i] = 0;
-            for (int u: group_claims[i][1])
-                possible[u] = false;
-        }
-    }
+	// Step 4. Figure out what each group is claiming
+	for (int i = 0;i < n;i++) for (int u: claims[i])
+		if (colour[i] == colour[leader[i]])
+			group_claims[leader[i]][1].insert(u);
+		else
+			group_claims[leader[i]][0].insert(u);
+	for (int i = 0;i < n;i++)
+	{
+		if (group_claims[i][0].size() > 1 && group_claims[i][1].size() > 1)
+			mistake();
+		if (group_claims[i][0].size() > 1)
+		{
+			type[i] = 1;
+			for (int u: group_claims[i][0])
+				possible[u] = false;
+		}
+		if (group_claims[i][1].size() > 1)
+		{
+			type[i] = 0;
+			for (int u: group_claims[i][1])
+				possible[u] = false;
+		}
+	}
 
-    // Step 5. Check out our force-moves
-    int forced = UNDEFINED;
-    for (int i = 0;i < n;i++) if (type[i] != UNDEFINED)
-    {
-        for (int accusor: belong[i])
-        {
-            bool i_am_forced = false;
-            if (type[i] == 1 && colour[accusor] == colour[i])
-                i_am_forced = true;
-            if (type[i] == 0 && colour[accusor] != colour[i])
-                i_am_forced = true;
-            if (i_am_forced)
-            {
-                for (int victim: claims[accusor]) if (possible[victim])
-                {
-                    if (forced != UNDEFINED && forced != victim)
-                        mistake();
-                    forced = victim;
-                }
-            }
-        }
-    }
-    if (forced != UNDEFINED)
-    {
-        printf("%d\n", forced+1);
-        return 0;
-    }
+	// Step 5. Check out our force-moves
+	int forced = UNDEFINED;
+	for (int i = 0;i < n;i++) if (type[i] != UNDEFINED)
+	{
+		for (int accusor: belong[i])
+		{
+			bool i_am_forced = false;
+			if (type[i] == 1 && colour[accusor] == colour[i])
+				i_am_forced = true;
+			if (type[i] == 0 && colour[accusor] != colour[i])
+				i_am_forced = true;
+			if (i_am_forced)
+			{
+				for (int victim: claims[accusor]) if (possible[victim])
+				{
+					if (forced != UNDEFINED && forced != victim)
+						mistake();
+					forced = victim;
+				}
+			}
+		}
+	}
+	if (forced != UNDEFINED)
+	{
+		printf("%d\n", forced+1);
+		return 0;
+	}
 
-    // Step 6. Check out the if-else branch groups that say, either take this or take that
-    // Any correct answer must be presented as an option in all of these if-else branches
-    int tot_occ = 0;
-    for (int i = 0;i < n;i++) if (group_claims[i][0].size() && group_claims[i][1].size())
-    {
-        for (int u: group_claims[i][0])
-            occurrences[u]++;
-        for (int u: group_claims[i][1])
-            occurrences[u]++;
-        tot_occ++;
-    }
-    set<int> options;
-    for (int i = 0;i < n;i++) if (occurrences[i] == tot_occ && possible[i])
-        options.insert(i);
+	// Step 6. Check out the if-else branch groups that say, either take this or take that
+	// Any correct answer must be presented as an option in all of these if-else branches
+	int tot_occ = 0;
+	for (int i = 0;i < n;i++) if (group_claims[i][0].size() && group_claims[i][1].size())
+	{
+		for (int u: group_claims[i][0])
+			occurrences[u]++;
+		for (int u: group_claims[i][1])
+			occurrences[u]++;
+		tot_occ++;
+	}
+	set<int> options;
+	for (int i = 0;i < n;i++) if (occurrences[i] == tot_occ && possible[i])
+		options.insert(i);
 
-    // Step 7. Output
-    for (int i: options)
-        printf("%d\n", i+1);
-    if (options.empty())
-        mistake();
-    return 0;
+	// Step 7. Output
+	for (int i: options)
+		printf("%d\n", i+1);
+	if (options.empty())
+		mistake();
+	return 0;
 }
