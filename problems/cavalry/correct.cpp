65a6ea9826db25a12f3c5111415b5f7a354ce612
Charles Jameson <cwj@dhcp-10-249-74-157.eduroam.wireless.private.cam.ac.uk>
2018-12-04 12:30:56 +0000

    removed files

--- problems/cavalry/correct.cpp
+++ problems/cavalry/correct.cpp
@@ -4,8 +4,6 @@ using namespace std;
 int n, a[100005];
 
 int main() {
-    freopen("cavalryin.txt", "r", stdin);
-    freopen("cavalryout.txt", "w", stdout);
     scanf("%d", &n);
     for (int i = 0, x; i < n; i++) scanf("%d", &x), a[x]++;
     for (int i = 1; i <= 100000; i++) if (a[i] % i) {
