f1ac08514552cbac894495a667699391b8a0fc8d
Charles Jameson <cwj@dhcp-10-249-79-161.eduroam.wireless.private.cam.ac.uk>
2019-04-26 16:03:37 +0100

    using python3 instead of python3.6

--- start.sh
+++ start.sh
@@ -15,8 +15,8 @@ tmux new-session -d -s $session
 
 tmux splitw -h -t 0 -p 30
 tmux splitw -v -t 1 -p 75
-tmux send-keys -t 1 "python3.6 src/server.py $PORT" C-m
-tmux send-keys -t 2 "python3.6 src/worker.py" C-m
+tmux send-keys -t 1 "python3 src/server.py $PORT" C-m
+tmux send-keys -t 2 "python3 src/worker.py" C-m
 tmux selectp -t 0
 
 tmux a -t app
