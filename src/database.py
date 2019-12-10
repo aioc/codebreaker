e4ebf91c538a569959429466c0de0676847e0405
Ubuntu <ubuntu@ip-172-31-31-147.us-west-2.compute.internal>
2019-12-10 16:14:07 +0000

    Add more database configuration to the setup script.

--- src/database.py
+++ src/database.py
@@ -1,5 +1,6 @@
 import asyncio
 import asyncpg
+import getpass
 import os
 
 
@@ -8,15 +9,10 @@ connection = None
 
 async def connect():
     global connection
-    dsn = os.environ.get('DATABASE_URL')
-    if dsn:
-        connection = await asyncpg.connect(dsn = dsn)
-    else:
-        connection = await asyncpg.connect(
-            user = 'postgres',
-            password = 'postgres',
-            database = 'codebreaker'
-        )
+    connection = await asyncpg.connect(
+        user = getpass.getuser(),
+        database = 'codebreaker'
+    )
 
 
 loop = asyncio.get_event_loop()
--- table_setup.sql
+++ table_setup.sql
@@ -1,3 +1,14 @@
+
+-- The user to to give database access to
+\set run_user 'ubuntunununu'
+
+-- Create codebreaker database, if it doesn't already exist.
+SELECT 'CREATE DATABASE codebreaker'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'codebreaker')\gexec
+
+\connect codebreaker
+
+-- Create tables
 CREATE TABLE IF NOT EXISTS users (
 	username VARCHAR UNIQUE,
 	password VARCHAR,
@@ -24,7 +35,15 @@ name VARCHAR UNIQUE,
 value INT
 );
 
+-- Do some initial setup
 INSERT INTO settings(name, value) SELECT 'contestant_access', 0
     WHERE NOT EXISTS (SELECT * FROM settings WHERE name='contestant_access');
 INSERT INTO settings(name, value) SELECT 'scoreboard_freeze_id', 100000000
     WHERE NOT EXISTS (SELECT * FROM settings WHERE name='scoreboard_freeze_id');
+
+-- Give permissions to the specified user
+CREATE USER :run_user;
+GRANT CONNECT ON DATABASE codebreaker TO :run_user;
+GRANT USAGE ON SCHEMA public TO :run_user;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO :run_user;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO :run_user;
