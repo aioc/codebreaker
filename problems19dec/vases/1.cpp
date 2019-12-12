d7c51e8707f3445660636a0d54ca5371d622d24d
Kevin Tran <kevinohappyface@gmail.com>
2019-12-12 13:19:18 +0000

    Initial draft of vases

---
+++ problems19dec/vases/1.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+    int N;
+    scanf("%d", &N);
+    
+    if(N <= 6) {
+        printf("%d %d %d\n", -1, -1, -1);
+    } else {
+        printf("%d %d %d\n", 1, 2, N-3);
+    }
+}
