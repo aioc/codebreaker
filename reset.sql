f723108565947dc8929ce5710171633d933ebd08
Kevin Tran <kevinohappyface@gmail.com>
2019-12-12 03:50:10 +0000

    Add warning, remove user info

--- reset.sql
+++ reset.sql
@@ -1,23 +1,7 @@
+-- WARNING: THIS SCRIPT WILL DELETE ALL USER DATA! DO NOT RUN DURING A CODEBREAKER CONTEST
 TRUNCATE users CASCADE;
 TRUNCATE results CASCADE;
 
 INSERT INTO users(username, password, visible, admin) VALUES ('tutors', 'simmeringaioli', FALSE, TRUE);
 
-INSERT INTO users(username, password, visible, admin) VALUES ('ralismark', 'apple$rocks#aio', TRUE, FALSE);
-INSERT INTO users(username, password, visible, admin) VALUES ('anonymous', 'brexitsucks', TRUE, FALSE);
-INSERT INTO users(username, password, visible, admin) VALUES ('tunanshi', 'f850ac51', TRUE, FALSE);
-INSERT INTO users(username, password, visible, admin) VALUES ('andrewkerr', 'andrewkerr', TRUE, FALSE);
-INSERT INTO users(username, password, visible, admin) VALUES ('leepiccowoflife', '70fe9912', TRUE, FALSE);
-INSERT INTO users(username, password, visible, admin) VALUES ('thyroid', 'ryanwang', TRUE, FALSE);
-INSERT INTO users(username, password, visible, admin) VALUES ('iiliffe', 'kettlediameter', TRUE, FALSE);
-INSERT INTO users(username, password, visible, admin) VALUES ('pony_joe1', 'Angusage14', TRUE, FALSE);
-INSERT INTO users(username, password, visible, admin) VALUES ('i_am_sb', 'iamsherlocked', TRUE, FALSE);
-INSERT INTO users(username, password, visible, admin) VALUES ('mckinleykeys', '393b9dbc', TRUE, FALSE);
-INSERT INTO users(username, password, visible, admin) VALUES ('mzhao', 'info1234', TRUE, FALSE);
-INSERT INTO users(username, password, visible, admin) VALUES ('jl58261', 'carrots21', TRUE, FALSE);
-INSERT INTO users(username, password, visible, admin) VALUES ('super_aiderton', '22scOne\', TRUE, FALSE);
-INSERT INTO users(username, password, visible, admin) VALUES ('rkh2002', 'AliMessi', TRUE, FALSE);
-INSERT INTO users(username, password, visible, admin) VALUES ('maayan', 'b7e3cd13', TRUE, FALSE);
-INSERT INTO users(username, password, visible, admin) VALUES ('steveplusplus', 'steveplusplus', TRUE, FALSE);
-INSERT INTO users(username, password, visible, admin) VALUES ('technocoder', 'fab30f42', TRUE, FALSE);
-INSERT INTO users(username, password, visible, admin) VALUES ('ivansolo2', 'acd64b16', TRUE, FALSE);
+INSERT INTO users(username, password, visible, admin) VALUES ('testuser', 'testpass', TRUE, FALSE);
