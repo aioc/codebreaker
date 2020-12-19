c5b30a7b426fd3d7f994ebb1cf2ba3314045d8bd
Kevin Tran <kevinohappyface@gmail.com>
2020-12-19 10:15:49 +0000

    More dec20 sanities

--- problems20dec/baubles/correct.cpp
+++ problems20dec/baubles/correct.cpp
@@ -2,8 +2,6 @@
 using namespace std;
 int a, b, s, c, d, ans1 = 2e9, ans2 = 2e9;
 int main() {
-	freopen("baublesin.txt", "r", stdin);
-	freopen("baublesout.txt", "w", stdout);
 	scanf("%d%d%d%d%d", &a, &b, &s, &c, &d);
 	if (c) ans1 = a+s-max(0, d-b)-c+1; // don't make red ones
 	if (d) ans2 = b+s-max(0, c-a)-d+1; // don't make blue ones
